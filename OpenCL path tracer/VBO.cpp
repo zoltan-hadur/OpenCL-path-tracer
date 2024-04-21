7d666b027eda6d2bbf846cbaa70dcae403269ed5
Zoltan Hadur <hadur.zoltan@gmail.com>
2024-04-21 20:16:02 +0200

    Add possibility to update VBO

--- OpenCL path tracer/VBO.cpp
+++ OpenCL path tracer/VBO.cpp
@@ -4,13 +4,28 @@ VBO::VBO(std::vector<Vertex> const& vertices)
 {
     glGenBuffers(1, &_id);
     Bind();
-    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * vertices.size(), vertices.data(), GL_STATIC_DRAW);
+    ReplaceVertices(vertices);
     Unbind();
 }
 
 GLuint VBO::Id() const
 {
     return _id;
+}
+
+void VBO::UpdateVertices(std::vector<Vertex> const& vertices) const
+{
+    if (vertices.size() != _size)
+    {
+        throw std::runtime_error("Size differs!");
+    }
+    glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(Vertex) * _size, vertices.data());
+}
+
+void VBO::ReplaceVertices(std::vector<Vertex> const& vertices)
+{
+    _size = vertices.size();
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * vertices.size(), vertices.data(), GL_STATIC_DRAW);
 }
 
 void VBO::Bind() const
--- OpenCL path tracer/VBO.h
+++ OpenCL path tracer/VBO.h
@@ -9,11 +9,15 @@ class VBO
 {
 private:
     GLuint _id;
+    std::size_t _size;
 
 public:
     VBO(std::vector<Vertex> const& vertices);
 
     GLuint Id() const;
+
+    void UpdateVertices(std::vector<Vertex> const& vertices) const;
+    void ReplaceVertices(std::vector<Vertex> const& vertices);
 
     void Bind() const;
     void Unbind() const;
