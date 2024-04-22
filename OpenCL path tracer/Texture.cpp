f7eb2874e0f57cd04209997e7ba02904a8742784
Zoltan Hadur <hadur.zoltan@gmail.com>
2024-04-22 23:08:04 +0200

    Change texture filter to linear

--- OpenCL path tracer/Texture.cpp
+++ OpenCL path tracer/Texture.cpp
@@ -10,8 +10,8 @@ Texture::Texture(GLint internalformat, GLsizei width, GLsizei height, GLenum for
     glGenTextures(1, &_id);
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, _id);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexImage2D(GL_TEXTURE_2D, 0, internalformat, width, height, 0, format, type, pixels);
