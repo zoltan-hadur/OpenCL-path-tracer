44d6bc5d5ce82aa26e574aff81c1a6ce6c0c18af
Zoltan Hadur <hadur.zoltan@gmail.com>
2024-05-19 21:10:39 +0200

    Refactor StringHelper::GetUnicodeCharacter Add StringHelper::GetUtf8String

--- OpenCL path tracer/StringHelper.cpp
+++ OpenCL path tracer/StringHelper.cpp
@@ -6,33 +6,95 @@
 
 namespace OpenCL_PathTracer
 {
-    std::vector<uint32_t> StringHelper::GetUnicodeCharacters(std::string text)
+    std::vector<uint32_t> StringHelper::GetUnicodeCharacters(std::string utf8text)
     {
-        auto characters = std::vector<uint32_t>();
-        int32_t character = 0;
-        for (int i = 0; i < text.length(); ++i)
+        auto unicodeCharacters = std::vector<uint32_t>();
+        int32_t unicodeCharacter = 0;
+        auto remainingBytesCount = 0;
+        for (uint8_t byte : utf8text)
         {
-            uint8_t c = text[i];
-            if (c >> 7 == 0)
+            if (remainingBytesCount > 0)
             {
-                character = c;
+                // 2nd/3rd/4th byte of utf8 encoded character
+                if (byte >> 6 == 0b10)
+                {
+                    unicodeCharacter = (unicodeCharacter << 6) | (byte & 0b00111111);
+                    remainingBytesCount--;
+                }
+                else
+                {
+                    throw std::runtime_error(std::format("Remaining bytes left: {0}. Byte '{1}' (0b{2}) should match mask 0b10xxxxxx as 2nd/3rd/4th byte of utf8 encoded character!", remainingBytesCount, byte, std::bitset<8>(byte).to_string()));
+                }
+            }
+            // Single byte
+            else if (byte >> 7 == 0b0)
+            {
+                unicodeCharacter = byte & 0b01111111;
+                remainingBytesCount = 0;
+            }
+            // 1st byte of 2 bytes
+            else if (byte >> 5 == 0b110)
+            {
+                unicodeCharacter = byte & 0b00011111;
+                remainingBytesCount = 1;
+            }
+            // 1st byte of 3 bytes
+            else if (byte >> 4 == 0b1110)
+            {
+                unicodeCharacter = byte & 0b00001111;
+                remainingBytesCount = 2;
+            }
+            // 1st byte of 4 bytes
+            else if (byte >> 3 == 0b11110)
+            {
+                unicodeCharacter = byte & 0b00000111;
+                remainingBytesCount = 3;
             }
             else
             {
-                auto count =
-                    c >> 5 == 0b110 ? 2 :
-                    c >> 4 == 0b1110 ? 3 :
-                    c >> 3 == 0b11110 ? 4 :
-                    throw std::runtime_error(std::format("Character '{0}' (0b{1}) is neither a single byte ascii nor the 1st byte of 2/3/4 byte character!", c, std::bitset<8>(c).to_string()));
-                character = (0b1111'1111 >> count + 1) & c;
-                for (int j = 1; j < count; ++j)
-                {
-                    c = text[++i];
-                    character = (character << 6) | (c & 0b0011'1111);
-                }
+                throw std::runtime_error(std::format("Byte '{0}' (0b{1}) is neither a single byte ascii nor the 1st byte of 2/3/4 byte utf8 encoded character!", byte, std::bitset<8>(byte).to_string()));
             }
-            characters.push_back(character);
+
+            if (remainingBytesCount == 0)
+            {
+                unicodeCharacters.push_back(unicodeCharacter);
+            }
         }
-        return characters;
+        return unicodeCharacters;
+    }
+
+    std::string StringHelper::GetUtf8String(std::vector<uint32_t> unicodeCharacters)
+    {
+        auto utf8string = std::string();
+        for (auto unicodeCharacter : unicodeCharacters)
+        {
+            // Single byte
+            if (unicodeCharacter <= 0x007F)
+            {
+                utf8string.push_back(unicodeCharacter & 0b01111111);
+            }
+            // 2 bytes
+            else if (unicodeCharacter <= 0x07FF)
+            {
+                utf8string.push_back(0b11000000 | ((unicodeCharacter & 0b00000000'00000111'11000000) >> 6));
+                utf8string.push_back(0b10000000 | ((unicodeCharacter & 0b00000000'00000000'00111111) >> 0));
+            }
+            // 3 bytes
+            else if (unicodeCharacter <= 0xFFFF)
+            {
+                utf8string.push_back(0b11100000 | ((unicodeCharacter & 0b00000000'11110000'00000000) >> 12));
+                utf8string.push_back(0b10000000 | ((unicodeCharacter & 0b00000000'00001111'11000000) >> 6));
+                utf8string.push_back(0b10000000 | ((unicodeCharacter & 0b00000000'00000000'00111111) >> 0));
+            }
+            // 4 bytes
+            else if (unicodeCharacter <= 0x10FFFF)
+            {
+                utf8string.push_back(0b11110000 | ((unicodeCharacter & 0b00011100'00000000'00000000) >> 18));
+                utf8string.push_back(0b10000000 | ((unicodeCharacter & 0b00000011'11110000'00000000) >> 12));
+                utf8string.push_back(0b10000000 | ((unicodeCharacter & 0b00000000'00001111'11000000) >> 6));
+                utf8string.push_back(0b10000000 | ((unicodeCharacter & 0b00000000'00000000'00111111) >> 0));
+            }
+        }
+        return utf8string;
     }
 }
--- OpenCL path tracer/StringHelper.h
+++ OpenCL path tracer/StringHelper.h
@@ -9,6 +9,7 @@ namespace OpenCL_PathTracer
     class StringHelper
     {
     public:
-        static std::vector<uint32_t> GetUnicodeCharacters(std::string text);
+        static std::vector<uint32_t> GetUnicodeCharacters(std::string utf8text);
+        static std::string GetUtf8String(std::vector<uint32_t> unicodeCharacters);
     };
 }
